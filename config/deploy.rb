830d54b6ec736968107bfdae667a41b49beffff1
DeLynn Berry <delynn@gmail.com>
2011-04-26 14:51:36 -0600

    Cleanup for our deploy environment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ branch = "pinnacol"
 user = "rails"
 sudo = false
 template = :pinnacol
+@passenger_deploy_path = "/var/www/passenger/ci"
+login_shell = true
 
 before_restarting_server do
 end
--- lib/inploy/templates/pinnacol.rb
+++ lib/inploy/templates/pinnacol.rb
@@ -5,9 +5,13 @@ module Inploy
         base.path = nil
       end
 
-      def local_setup
-        super
-        run "ln -s #{application_path}/public #{passenger_deploy_path}"
+      def remote_setup
+        remote_run "rm -rf #{application_path}"
+        remote_run "rm -r #{passenger_deploy_path}"
+        remote_run "cd #{path} && #{sudo_if_should}git clone --depth 1 #{repository} #{application}"
+        remote_run "cd #{application_path} && #{checkout}"
+        remote_run "cd #{application_path} && #{bundle}"
+        remote_run "ln -s #{application_path}/public #{passenger_deploy_path}"
       end
 
       def path
@@ -16,7 +20,15 @@ module Inploy
 
       private
         def passenger_deploy_path
-          @passenger_deploy_path || "/var/www/passenger/#{application}"
+          @passenger_deploy_path || "/var/www/passenger/#{application_folder}"
+        end
+
+        def checkout
+          branch.eql?("master") ? "" : "git checkout -f -b #{branch} origin/#{branch}"
+        end
+
+        def bundle
+          "#{bundle_cmd}" if using_bundler?
         end
       # end private
     end
